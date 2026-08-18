cbd11f21bb1336c92ae7c6f0b6b3c23d0ca56a10
SDEFTTH <sdeftthwl@gmail.com>
2026-08-18 16:39:43 +0530

    Fix datetime import in parse_date function

--- report_processor.py
+++ report_processor.py
@@ -63,7 +63,7 @@ def _run_report_body(SRC, OUT, HTML_OUT):
     MONTHS = {"JAN":1,"FEB":2,"MAR":3,"APR":4,"MAY":5,"JUN":6,"JUL":7,"AUG":8,"SEP":9,"OCT":10,"NOV":11,"DEC":12}
 
     def parse_date(v):
-        if isinstance(v, datetime.datetime):
+        if isinstance(v, datetime):
             return v.date()
         if isinstance(v, datetime.date):
             return v
